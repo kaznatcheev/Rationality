98e3b98d999d666720b1bee57df28ca0213350e7
Artem Kaznatcheev <akazna@lab7-23.CS.McGill.CA>
2013-01-25 16:34:19 -0500

    modified densityPlot

--- densityPlot.m
+++ densityPlot.m
@@ -1,8 +1,8 @@
-function densityPlot(data, boundaries, n_bins)
+function densityPlot(data, boundaries, game_point, n_bins)
 %densityPlot(data, boundaries, n_bins)
 %
 
-if (nargin < 3) || isempty(n_bins),
+if (nargin < 4) || isempty(n_bins),
     n_bins = floor(sqrt(length(data)));
 end;
 
@@ -20,5 +20,22 @@ pcolor(xi,yi,z);
 caxis([0 0.5*length(data)]);
 colormap(flipud(gray));
 shading interp;
+
+hold on;
+fplot(@(x) 0, [boundaries(1), boundaries(2)], 'k');
+fplot(@(x) 1, [boundaries(1), boundaries(2)], 'k');
+fplot(@(x) x, [boundaries(1), boundaries(2)], 'k');
+plot([0, 0], [boundaries(3), boundaries(4)], 'k');
+axis(boundaries);
+
+if (nargin >= 3) && ~isempty(game_point),
+    plot(game_point(1), game_point(2), ... 
+        '*', 'LineWidth',2,...
+        'MarkerEdgeColor','r', ...
+        'MarkerSize',15);
+end;
+
+%hold off;
+
 end
 
