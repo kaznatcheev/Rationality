559df1e0fdd8f747fd48f56acdff2cacdbe28144
Artem Kaznatcheev <akazna@lab7-23.CS.McGill.CA>
2013-01-25 15:59:44 -0500

    made densityPlot

--- densityPlot.m
+++ densityPlot.m
@@ -3,7 +3,7 @@ function densityPlot(data, boundaries, n_bins)
 %
 
 if (nargin < 3) || isempty(n_bins),
-    n_bins = 10;
+    n_bins = floor(sqrt(length(data)));
 end;
 
 if (nargin < 2) || isempty(boundaries),
@@ -13,12 +13,12 @@ end;
 xi = linspace(boundaries(1), boundaries(2), n_bins);
 yi = linspace(boundaries(3), boundaries(4), n_bins);
 
-xr = interp1(xi,1:numel(xi),data(:,1),'nearest')';
-yr = interp1(yi,1:numel(yi),data(:,2),'nearest')';
+C = {xi, yi};
 
-z = accumarray([xr yr], 1, [n_bins n_bins]);
-
-surf(z);
-
+z = hist3(data,C);
+pcolor(xi,yi,z);
+caxis([0 0.5*length(data)]);
+colormap(flipud(gray));
+shading interp;
 end
 
