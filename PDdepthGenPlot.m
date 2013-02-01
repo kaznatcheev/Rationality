503e96b60a4fa513b1fd2b2e7e37cedd240ac5a7
Artem Kaznatcheev <kaznatcheev.artem@gmail.com>
2013-02-01 17:54:59 -0500

    Added image save functionality to PDdepthGenPlot.m

--- PDdepthGenPlot.m
+++ PDdepthGenPlot.m
@@ -1,7 +1,7 @@
 close all;
 
-PDrange = 0.2:0.1:0.9;
-runs = 1:5;
+PDrange = 0.3:0.1:0.9;
+runs = 1:10;
 
 for offset = PDrange,
     game_point = [-offset,1 + offset];
@@ -12,7 +12,12 @@ for offset = PDrange,
         genotypes(((run - 1)*500 + 1):(run*500),:) = dlmread(strcat(game_dir,'/nv',int2str(run),'/genotypes2000.txt'));
     end;
     
-    densityPlot(genotypes,[-2 2 -1 3],[],[],1);
-    figure;
+    h = densityPlot(genotypes,[-2 2 -1 3],[],[],1);
+    title(strcat('Density plot of genotypes at epoch 2000 (U=', num2str(game_point(1)), ', V=', num2str(game_point(2)), ')'));
+    print(h,'-dpng',strcat('../RationalityData/images/genotypes', int2str(10*offset), '.png'));
+    
+    h = figure;
     bar(gameTypeCount(genotypes));
+    title(strcat('Game type count of of genotypes at epoch 2000 (U=', num2str(game_point(1)), ', V=', num2str(game_point(2)), ')'));
+    print(h,'-dpng',strcat('../RationalityData/images/gamebar', int2str(10*offset), '.png'));
 end;
