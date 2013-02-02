0d5100c39781ccec112139561070748d8948c820
Artem Kaznatcheev <kaznatcheev.artem@gmail.com>
2013-02-02 04:02:48 -0500

    Added code to save coopRuns

--- drawDGConf.m
+++ drawDGConf.m
@@ -2,13 +2,13 @@ close all;
 
 %runs = [2 3 4 5 7 8 9 10];
 %runs = [ 2 4 5 7 8 10];
-runs = 1:5;
+runs = 1:10;
 i = 1;
 
 h = figure;
 set(h, 'Position', [0 0 300 900]);
 
-for offset = [0.2,0.5,0.6],
+for offset = [0.3,0.9],
     game_point = [-offset,1 + offset];
     game_dir = strcat('../RationalityData/U', int2str(10*game_point(1)), 'V', int2str(10*game_point(2)));
     
@@ -18,7 +18,7 @@ for offset = [0.2,0.5,0.6],
         all_data(:,:,run,i) = dlmread(strcat(game_dir,'/bv',int2str(run),'/data.txt'));
     end;
     
-    subplot(3,1,i);
+    subplot(2,1,i);
     densityPlot(genotypes,[-2 2 -1 3],[],40,0,0);
     %caxis([0 120]);
     %title(strcat('Density plot of genotypes at epoch 2000 (U=', num2str(game_point(1)), ', V=', num2str(game_point(2)), ')'));
@@ -32,16 +32,13 @@ axes('Position', [0.13 0.03 0.77 0.8], 'Visible', 'off');
 %caxis([0 120]);
 colorbar('SouthOutside');
 
-figure;
+h = figure;
 hold on;
 for run = runs,
     prop_coop = plotCoop(all_data(:,:,run,1),[],[],0);
     plot(prop_coop,'b');
     
     prop_coop = plotCoop(all_data(:,:,run,2),[],[],0);
-    plot(prop_coop,'g');
-    
-    prop_coop = plotCoop(all_data(:,:,run,3),[],[],0);
     plot(prop_coop,'r');
 end;
 
@@ -51,3 +48,5 @@ plot([0, max_epoch], [epsilon, epsilon], 'k--');
 plot([0, max_epoch], [1 - epsilon, 1 - epsilon], 'k--');
 axis([0, max_epoch, 0, 1]);
 hold off;
+
+print(h,'-dpng',strcat('../RationalityData/images/coopRuns.png'));
