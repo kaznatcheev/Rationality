7b725e1d17d52ffefad957a6ce4238762733e0a9
Artem Kaznatcheev <kaznatcheev.artem@gmail.com>
2013-05-13 16:45:52 -0400

    started simpleRun

---
+++ simpleRun.m
@@ -0,0 +1,10 @@
+function [ output_args ] = simpleRun(n_agents,n_epochs)
+%The goal of simpleRun is to provide a way to quickly test run the code
+%and look at its outputs
+
+%First we make a random graph of degree 3
+adjmx = full(createRandRegGraph(n_agents, 3));
+
+
+end
+
