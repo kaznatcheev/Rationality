8ac7c3b1f3163f01ccd6de8795f154fee4e9231a
Marcel MONTREY <marcel.montrey@mail.mcgill.ca>
2013-07-02 20:48:23 -0400

    Fixed alphaPDdepthPlot

--- alphaPDdepthPlot.m
+++ alphaPDdepthPlot.m
@@ -16,37 +16,33 @@ if(nargin < 3) || isempty(PD_depths),
     PD_depths = 0.1:0.1:1.0;
 end;
 
-for PD_depth = PD_depths,
-        %Find which folder we're opening.
-        game_point = [-PD_depth, 1 + PD_depth];
+average = zeros(length(PD_depths),1);
+std_error = zeros(length(PD_depths), 1);
+for i = 1:length(PD_depths),
+        game_point = [-PD_depths(i), 1 + PD_depths(i)];
         name_start = strcat('../RationalityData/U', int2str(10 * game_point(1)), 'V', int2str(10 * game_point(2)), suffix);
         
-        %Create a vector to hold each run's average alpha value across the
-        %window of timesteps.
         alphas = zeros(length(runs),1);
-        
-        %Average and store the alpha values across the window of timesteps
-        %for each run.
         for run = runs,
             data = dlmread(strcat(name_start, int2str(run), 'e', int2str(e_value), '/data.txt'));
             alphas(run) = mean(data(window, 12));
         end;
         
-        %Get the standard error for the average alpha values across all
-        %runs.
-        stderr = std(alphas)/sqrt(length(alphas));
-        
-        %Plot the results.
-        plot(alphas - stderr, 'b');
-        hold on;
-        plot(alphas + stderr, 'b');
-        hold off;
-        xlabel('Run');
-        if(k == 1),
-            ylabel(strcat('Average alpha'));
-        elseif(k == 2),
-            ylabel(strcat('Proportion of agents using alpha'));
-        end;
+        average(i) = mean(alphas);
+        std_error(i) = std(alphas) / sqrt(length(alphas));
+end;
+
+%Plot the results.
+figure;
+plot(average - std_error, 'b');
+hold on;
+plot(average + std_error, 'b');
+hold off;
+xlabel('UV Depth');
+if(k == 1),
+    ylabel(strcat('Average alpha'));
+elseif(k == 2),
+    ylabel(strcat('Proportion of agents using alpha'));
 end;
 
 end
