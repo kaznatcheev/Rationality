66e7ceab1cc1c41a583198f97daa04c85699bcfe
Artem Kaznatcheev <kaznatcheev.artem@gmail.com>
2014-01-10 16:01:11 -0500

    Created PDdepthNicePlot for CogSci2014
    The goal is to be able to create a single density plot with two different parameter settings on it to increase data density in the figure.

---
+++ PDdepthNicePlot.m
@@ -0,0 +1,32 @@
+close all;
+
+runs = 1:10;
+
+offset_red = 0.3;
+offset_blue = 0.9;
+gp_red = [-offset_red,1 + offset_red];
+gp_blue = [-offset_blue,1 + offset_blue];
+
+%add red
+game_dir = strcat('../RationalityData/U', int2str(10*gp_red(1)), 'V', int2str(10*gp_red(2)));
+
+genotypes = zeros(500*length(runs),2);
+for run = runs,
+   genotypes(((run - 1)*500 + 1):(run*500),:) = dlmread(strcat(game_dir,'/bv',int2str(run),'/genotypes2000.txt'));
+end;
+    
+h = densityPlot(genotypes,[-2 2 -1 3],[],[],0,1);
+title('Density plot of genotypes at epoch 2000');
+
+%add blue
+game_dir = strcat('../RationalityData/U', int2str(10*gp_blue(1)), 'V', int2str(10*gp_blue(2)));
+
+genotypes = zeros(500*length(runs),2);
+for run = runs,
+   genotypes(((run - 1)*500 + 1):(run*500),:) = dlmread(strcat(game_dir,'/bv',int2str(run),'/genotypes2000.txt'));
+end;
+
+h = densityPlot(genotypes,[-2 2 -1 3],[],[],0,0);
+%title('Density plot of genotypes at epoch 2000');
+
+print(h,'-dpng',strcat('../RationalityData/images/genotypesRedBlue.png'));
